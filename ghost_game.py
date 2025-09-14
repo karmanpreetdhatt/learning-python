1bb61df54274a189cf57883addcf52792b048481
Karmanpreet Dhatt <karmanpreetdhatt@gmail.com>
2025-09-14 16:19:10 +0530

    Fun Ghost Game

---
+++ ghost_game.py
@@ -0,0 +1,24 @@
+# Ghost Game
+from random import randint
+
+print("Ghost Game")
+feeling_brave = True
+score = 0
+while feeling_brave:
+    ghost_door = randint(1, 3)
+    print("Three doors ahead...")
+    print("A ghost behind one...")
+    print("Which door do you want to open?")
+    door = input(
+        """1, 2 or 3?
+    Type your answer here:  """
+    )
+    door_num = int(door)
+    if door_num == ghost_door:
+        print("GHOST! GHOST! Aaah! 😱😱😱")
+        feeling_brave = False
+    else:
+        print("No ghost! THANK GOD! 🥲")
+        print("You are lucky! ☺️☺️")
+print("Run! Quick, quick! Oh, quuuuuuuuiiiiiiiiiccckk!")
+print(f"Game over!")
