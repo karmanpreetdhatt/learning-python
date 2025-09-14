579107ac943c97681bb2c817ef3de61a168cd026
Karmanpreet Dhatt <karmanpreetdhatt@gmail.com>
2025-09-14 17:08:33 +0530

    Changed formatting

--- variables2.py
+++ variables2.py
@@ -1,10 +1,10 @@
-youtuber_name = 'Mr. Beast'
+youtuber_name = "Mr. Beast"
 
-print('WELCOME TO THE YOUTUBER GAME!')
-print('The most popular youtuber...')
-print(f'...is {youtuber_name}!')
+print("WELCOME TO THE YOUTUBER GAME!")
+print("The most popular youtuber...")
+print(f"...is {youtuber_name}!")
 print()
-print('He has 100 million subscribers! Oh my ache, that is a lot of subscribers! ')
+print("He has 100 million subscribers! Oh my ache, that is a lot of subscribers! ")
 KeyboardInterrupt
 KeyError
 GeneratorExit
