0c732e19e683ab95dabb1927078cca45d1712523
Karmanpreet Dhatt <karmanpreetdhatt@gmail.com>
2025-09-14 16:22:56 +0530

    Math functions

---
+++ maths.py
@@ -0,0 +1,25 @@
+from math import sqrt
+
+def addition(num1, num2):
+    result = num1+num2
+    return result
+
+def subtraction(num1, num2):
+    result = num1-num2
+    return result
+
+def multiplication(num1, num2):
+    result = num1*num2
+    return result
+
+def division(num1, num2):
+    result = num1/num2
+    return result
+
+def square(num):
+    result = num * num
+    return result
+
+def square_root(num):
+    result = sqrt(num)
+    return result    
