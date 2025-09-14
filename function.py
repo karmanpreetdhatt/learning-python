4ac973c77846b22b36b19da789140d1a9e0030b8
Karmanpreet Dhatt <karmanpreetdhatt@gmail.com>
2025-09-14 16:17:34 +0530

    Testing math functions

---
+++ function.py
@@ -0,0 +1,8 @@
+from maths import addition, subtraction, division, multiplication
+
+num1 = int(input("Enter the 1st number: "))
+num2 = int(input("Enter the 2nd number: "))
+
+result = addition(num1, num2)
+
+print(result)
