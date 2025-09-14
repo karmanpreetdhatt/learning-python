88a4a70e5809eddfc8b284cd7714d894635c59a5
Karmanpreet Dhatt <karmanpreetdhatt@gmail.com>
2025-09-14 16:26:09 +0530

    Fun Rock, paper, scissors game

---
+++ rps.py
@@ -0,0 +1,53 @@
+from random import randint
+
+rock = 0
+paper = 1
+scissors = 2
+
+winner = ""
+
+computer_guess = randint(0, 2)
+
+print("Rock, paper, scissor, shoot!")
+
+user_guess = (
+    int(
+        input(
+            """Please pick one:
+                       1. Rock
+                       2. Paper
+                       3. Scissors
+                       """
+        )
+    )
+    - 1
+)
+
+print(f"Computer guess: {computer_guess}")
+print(f"User guess: {user_guess}")
+
+if user_guess == rock:
+    if computer_guess == rock:
+        winner = "draw"
+    elif computer_guess == paper:
+        winner = "computer"
+    else:
+        winner = "user"
+
+elif user_guess == paper:
+    if computer_guess == rock:
+        winner = "computer"
+    elif computer_guess == paper:
+        winner = "draw"
+    else:
+        winner = "computer"
+
+elif user_guess == scissors:
+    if computer_guess == rock:
+        winner = "computer"
+    elif computer_guess == paper:
+        winner = "user"
+    else:
+        winner = "draw"
+
+print(f"The winner is {winner} 🕶️")
