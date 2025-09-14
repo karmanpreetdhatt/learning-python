14a4ac4af29945e6879b3a7ce9101af1e43e0412
Karmanpreet Dhatt <karmanpreetdhatt@gmail.com>
2025-09-14 16:37:19 +0530

    The shocking thing about variables

---
+++ variables2.py
@@ -0,0 +1,12 @@
+youtuber_name = 'Mr. Beast'
+
+
+print('The most popular youtuber...')
+print(f'...is {youtuber_name}!')
+print()
+print('He has 100 million subscribers! Oh my ache, that is a lot of subscribers! ')
+KeyboardInterrupt
+KeyError
+GeneratorExit
+ExceptionGroup
+getattr
