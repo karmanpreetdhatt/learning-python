d36d987450d52d37d29a455e59bf88ee4bbbed61
Karmanpreet Dhatt <karmanpreetdhatt@gmail.com>
2025-10-20 16:47:58 +0530

    Changed
    I decreased 50 numbers of the table

--- table.py
+++ table.py
@@ -1,7 +1,7 @@
 print("Welcome to Karman's multiplication table generator!")
 num = int(input("Enter a number and I will print a table for it: "))
 
-for i in range(100):
+for i in range(50):
     m = i + 1
     product = num * m
     print(f"{num} * {m} = {product}")
